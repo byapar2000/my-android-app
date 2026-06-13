86842f8b576aaa30f43dbd10600d99ce19a32243
byapar2000 <byapar@protonmail.com>
2026-06-13 17:17:10 +0545

    initial android app

---
+++ app/src/main/java/com/myapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.myapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView tv = new TextView(this);
+        tv.setText("Hello APK from Termux 🚀");
+        setContentView(tv);
+    }
+}
